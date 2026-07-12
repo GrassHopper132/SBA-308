e4aeac481440d7c86b13c1b5f06189b0b56aa58e
GrassHopper132 <crhymer101@gmail.com>
2026-07-12 14:31:14 -0400

    Add calculateFinalScore function for score calculation

--- SBA307.js
+++ SBA307.js
@@ -3,3 +3,6 @@ function isAssignmentDue(dueAtStr) {
     const currentDate = new Date(); 
     return dueAtDate <= currentDate;
 }
+function calculateFinalScore(submission, assignment) {
+    const dueDate = new Date(assignment.due_at);
+    const submissionDate = new Date(submission.submitted_at);
